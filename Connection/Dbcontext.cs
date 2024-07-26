4225909bfa30c9d5ef2032e14dfbd5ac7006c2aa
miraa-said <miraasaid16@gmail.com>
2024-07-26 14:51:04 +0300

    this my first changes

---
+++ Connection/Dbcontext.cs
@@ -0,0 +1,13 @@
+﻿namespace Said_Project.Connection;
+using Microsoft.EntityFrameworkCore;
+using Said_Project.Entities;
+
+public class MyDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=mydatabase.db");
+    }
+}
